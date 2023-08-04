5761a16e865060d5e418efc9be91c40875e250fe
hugostanley <stanleyhugo06@gmail.com>
2023-08-04 13:32:30 +0800

    chore: make instance variables available ot all

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,11 +1,21 @@
 class HabitsController < ApplicationController
   before_action :authenticate_user!
+  before_action :instantiate_data
   def index
-    @habits = Habit.where(user_id: current_user.id)
   end
 
   def previewer
-    @date_today = Time.now
     @mode = params[:view]
+  end
+
+  def new
+    @habit = Habit.new
+  end
+
+  private
+
+  def instantiate_data
+    @date_today = Time.now
+    @habits = Habit.where(user_id: current_user.id)
   end
 end
