91066763d8317dede94874f8946d86caceb7598b
hugostanley <stanleyhugo06@gmail.com>
2023-08-04 20:14:26 +0800

    feat: create method for new habit creation

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,6 +1,7 @@
 class HabitsController < ApplicationController
   before_action :authenticate_user!
   before_action :instantiate_data
+
   def index
   end
 
@@ -12,10 +13,25 @@ class HabitsController < ApplicationController
     @habit = Habit.new
   end
 
+  def create
+    @habit = Habit.new(new_habits_params)
+
+    respond_to do |format|
+      # looks for a turbo_stream template
+      #
+      # located at: views/habits/create.turbo_stream.erb
+      format.turbo_stream if @habit.save
+    end
+  end
+
   private
 
   def instantiate_data
     @date_today = Time.now
     @habits = Habit.where(user_id: current_user.id)
+  end
+
+  def new_habits_params
+    params.require(:habit).permit(:user_id, :name, :description, :habit_type, :frequency)
   end
 end
