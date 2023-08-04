82a6961a7829cfdffa407fc55a9dc643d001e0ef
hugostanley <stanleyhugo06@gmail.com>
2023-08-05 00:19:14 +0800

    chore: create empty methods and comments as TOOD

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -4,4 +4,17 @@ class Habit < ApplicationRecord
   validates :description, length: {maximum: 500}
   validates :habit_type, inclusion: {in: %w[daily weekly monthly], message: "%<value>s is not a valid type"}
   validates :frequency, numericality: {only_integer: true}
+  has_many :habit_logs
+
+  # GET how many is accomplished today 
+  def logs_for_today
+  end
+
+  # GET how many is accomplished this week 
+  def logs_for_this_week
+  end
+
+  # GET how many is accomplished this month 
+  def logs_for_this_mongth
+  end
 end
--- app/models/habit_log.rb
+++ app/models/habit_log.rb
@@ -1,4 +1,6 @@
 class HabitLog < ApplicationRecord
   belongs_to :user
   belongs_to :habit
+
+  # create a callback to check if the frequency per day is not exceeded yet
 end
