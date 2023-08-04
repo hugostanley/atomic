385266c49ed2106d41c894310d3200f8ea19d0ad
hugostanley <stanleyhugo06@gmail.com>
2023-08-05 00:11:05 +0800

    feat: create a habit_log model

---
+++ app/models/habit_log.rb
@@ -0,0 +1,4 @@
+class HabitLog < ApplicationRecord
+  belongs_to :user
+  belongs_to :habit
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_04_112959) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_04_155641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "habit_logs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "habit_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["habit_id"], name: "index_habit_logs_on_habit_id"
+    t.index ["user_id"], name: "index_habit_logs_on_user_id"
+  end
 
   create_table "habits", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -42,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_04_112959) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "habit_logs", "habits"
+  add_foreign_key "habit_logs", "users"
   add_foreign_key "habits", "users"
 end
