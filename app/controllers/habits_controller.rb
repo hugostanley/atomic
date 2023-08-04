8f261c274e61577af8b821008e6243aeff42a0ed
hugostanley <stanleyhugo06@gmail.com>
2023-08-04 12:13:55 +0800

    feat: create methods for habits controlller

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -1,2 +1,11 @@
 class HabitsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @habits = Habit.where(user_id: current_user.id)
+  end
+
+  def previewer
+    @date_today = Time.now
+    @mode = params[:view]
+  end
 end
