7ef8712309ae503eeb35616ba9dc351a1a8c1472
hugostanley <stanleyhugo06@gmail.com>
2023-08-04 20:46:03 +0800

    chore: make habit_type enums lowercase

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -2,6 +2,6 @@ class Habit < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, length: {maximum: 100}
   validates :description, length: {maximum: 500}
-  validates :habit_type, inclusion: {in: %w[Daily Weekly Monthly], message: "%<value>s is not a valid type"}
+  validates :habit_type, inclusion: {in: %w[daily weekly monthly], message: "%<value>s is not a valid type"}
   validates :frequency, numericality: {only_integer: true}
 end
