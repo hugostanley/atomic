d2a4e3f570ddbaf257d308dbc905022123a5fc88
hugostanley <stanleyhugo06@gmail.com>
2023-08-04 14:04:32 +0800

    styles: add pop up and down animation to new habit form

---
+++ app/javascript/controllers/new_habit_form_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="new-habit-form"
+export default class extends Controller {
+  static targets = ['newHabitBtn', 'bgOverlay', 'popUpForm']
+  connect() {
+  }
+
+  openForm() {
+    this.bgOverlayTarget.classList.remove('hidden')
+    this.popUpFormTarget.classList.remove('close-form')
+    this.popUpFormTarget.classList.add('open-form')
+  }
+
+  closeForm(e) {
+    if (e.target == this.bgOverlayTarget) {
+      this.popUpFormTarget.classList.remove('open-form')
+      this.popUpFormTarget.classList.add('close-form')
+      setTimeout(() => {
+        this.bgOverlayTarget.classList.add('hidden')
+      }, [100])
+    }
+  }
+
+}
