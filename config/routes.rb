a758eeaccd152b06c9d2d1b64889d20a8fea9309
hugostanley <stanleyhugo06@gmail.com>
2023-08-05 11:50:44 +0800

    chore: add route for habit_log post

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'habits#index'
   get '/:view', to: 'habits#previewer', defaults: { view: 'daily' }, as: 'previewer'
   post '/habits', to: 'habits#create', as: 'create_habit'
+  post '/habit/:id', to: 'habits#log_habit', as: 'log_habit'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
