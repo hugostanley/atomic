afd9e0b7c53191ce1b11df17a2d7c47b515c47d4
hugostanley <stanleyhugo06@gmail.com>
2023-08-07 16:03:03 +0800

    add routes for edit and delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'habits#index'
   get '/:view', to: 'habits#previewer', defaults: { view: 'daily' }, as: 'previewer'
+  get '/habits/edit', to: 'habits#edit', as: 'edit_habits'
   post '/habits', to: 'habits#create', as: 'create_habit'
   post '/habit/:id', to: 'habits#log_habit', as: 'log_habit'
+  delete '/habit/:id', to: 'habits#delete_habit', as: 'delete_habit'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
