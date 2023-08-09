c5fdeb97a840052062d4277f9fd97f3807221970
hugostanley <stanleyhugo06@gmail.com>
2023-08-09 23:40:13 +0800

    feat add drag controller

---
+++ app/javascript/controllers/drag_list_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+import Rails from '@rails/ujs'
+import Sortable from 'sortablejs'
+
+// Connects to data-controller="drag-list"
+export default class extends Controller {
+  initialize(){
+  }
+
+  connect() {
+   this.sortable =  Sortable.create(this.element, {
+      onEnd: this.end.bind(this)
+    })
+  }
+
+  end(e){
+    const id = e.item.dataset.id
+    const data = new FormData()
+    data.append("position", e.newIndex + 1)
+    Rails.ajax({
+      url: `/move/habit/${id}`,
+      type: 'PATCH',
+      data
+    })
+  }
+}
