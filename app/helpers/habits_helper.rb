efc0d87f6c3d2d29a572bb61e9e99e403f2aa555
hugostanley <stanleyhugo06@gmail.com>
2023-08-09 11:44:31 +0800

    chore: move helper method to view helper

--- app/helpers/habits_helper.rb
+++ app/helpers/habits_helper.rb
@@ -6,4 +6,18 @@ module HabitsHelper
     return 4 if date.day > 28
     (date.day / 7.0).ceil
   end
+
+  def get_week_range(week_number)
+    date = Time.now
+    case week_number
+    when 1
+      Time.new(date.year, date.month, 1)..Time.new(date.year, date.month, 7)
+    when 2
+      Time.new(date.year, date.month, 8)..Time.new(date.year, date.month, 14)
+    when 3
+      Time.new(date.year, date.month, 15)..Time.new(date.year, date.month, 21)
+    when 4
+      Time.new(date.year, date.month, 22)..Time.new(date.year, date.month, date.end_of_month.day)
+    end
+  end
 end
