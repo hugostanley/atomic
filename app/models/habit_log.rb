aa02e9164e322005b00686a67a9d42db7b5a78a7
hugostanley <stanleyhugo06@gmail.com>
2023-08-09 11:45:17 +0800

    feat: create method for showing current week report

--- app/models/habit_log.rb
+++ app/models/habit_log.rb
@@ -17,6 +17,18 @@ class HabitLog < ApplicationRecord
     { total_frequency:, total_logs:, report_percentage: (total_logs / total_frequency.to_f * 100).round(2) }
   end
 
+  def self.get_weekly_report(user_id, range)
+    logs = HabitLog
+           .select('habit_logs.id as habit_log_id, habits.id as habit_id, atomic_users.id as user_id, habit_logs.created_at')
+           .joins(:habit, :user)
+           .where(habit_logs: { created_at: range }, habits: { habit_type: 'weekly' }, atomic_users: {id: user_id})
+    total_logs = logs.length
+    habits = Habit.where(user_id:,
+                         habit_type: 'weekly')
+    total_frequency = habits.reduce(0) { |sum, habit| sum + habit.frequency }
+    { total_frequency:, total_logs:, report_percentage: (total_logs / total_frequency.to_f * 100).round(2) }
+  end
+
   # TO BE FINALIZED
   # def self.get_daily_report_of_the_month(user_id)
   #   today = Time.zone.now
