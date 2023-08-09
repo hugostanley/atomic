1dc99c63d0fb7b1f1bd9d3ee1e985a860e910292
hugostanley <stanleyhugo06@gmail.com>
2023-08-09 12:21:13 +0800

    feat: display report graph

---
+++ app/javascript/controllers/report_graph_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="report-graph"
+export default class extends Controller {
+  static values = {
+    percentage: Number
+  }
+
+  connect() {
+    console.log(this.percentageValue)
+    if (this.percentageValue < 25) {
+      this.element.style.backgroundColor = 'rgb(187 247 208'
+      return
+    } else if (this.percentageValue < 50) {
+      this.element.style.backgroundColor = 'rgb(22 163 74)'
+      return
+    } else if (this.percentageValue < 75) {
+      this.element.style.backgroundColor = 'rgb(22 101 52)'
+      return
+    } else if(this.percentageValue <= 100) {
+      this.element.style.backgroundColor = 'rgb(5 46 22)'
+      return
+    }
+  }
+
+}
