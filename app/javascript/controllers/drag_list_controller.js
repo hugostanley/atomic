049033f2f2c4ee6f379ebc4efca10412c9356bde
hugostanley <stanleyhugo06@gmail.com>
2023-08-23 23:36:25 +0800

    chore: disable drag feature

--- app/javascript/controllers/drag_list_controller.js
+++ app/javascript/controllers/drag_list_controller.js
@@ -3,24 +3,25 @@ import Rails from '@rails/ujs'
 import Sortable from 'sortablejs'
 
 // Connects to data-controller="drag-list"
+// DISABLE FOR NOW!!!
 export default class extends Controller {
   initialize(){
   }
 
   connect() {
-   this.sortable =  Sortable.create(this.element, {
-      onEnd: this.end.bind(this)
-    })
+   //this.sortable =  Sortable.create(this.element, {
+   //   onEnd: this.end.bind(this)
+   // })
   }
 
   end(e){
-    const id = e.item.dataset.id
-    const data = new FormData()
-    data.append("position", e.newIndex + 1)
-    Rails.ajax({
-      url: `/move/habit/${id}`,
-      type: 'PATCH',
-      data
-    })
+    //const id = e.item.dataset.id
+    //const data = new FormData()
+    //data.append("position", e.newIndex + 1)
+    //Rails.ajax({
+    //  url: `/move/habit/${id}`,
+    //  type: 'PATCH',
+    //  data
+    //})
   }
 }
